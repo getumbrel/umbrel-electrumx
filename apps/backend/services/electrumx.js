19ba00de85cf3cfb6b1670b96a5d602ccf64c670
nmfretz <nmfretz@gmail.com>
2024-11-14 12:02:26 +1000

    Clean up electrum rpc code and add comments

--- apps/backend/services/electrumx.js
+++ apps/backend/services/electrumx.js
@@ -3,6 +3,7 @@ const bitcoindService = require("services/bitcoind");
 
 const constants = require("utils/const.js");
 
+// ElectrumX RPC details: https://electrumx.readthedocs.io/en/latest/rpc-interface.html
 class ElectrumClient {
   constructor(host, port) {
     this.client = new jayson.Client.tcp({
@@ -24,23 +25,26 @@ class ElectrumClient {
   }
 }
 
-// Single instance pattern
 const electrumClient = new ElectrumClient(constants.ELECTRUM_HOST, constants.ELECTRUM_RPC_PORT);
 
 async function getVersion() {
+  // Returns version number from response format: "{name} {semver}"
+  // e.g., "ElectrumX 1.16.0" -> "1.16.0"
   const info = await electrumClient.request('getinfo');
-  return info.version.substring(info.version.indexOf(' ') + 1) ?? 'unknown';
+  return info?.version?.split(' ')[1] ?? 'unknown';
 }
 
 async function syncPercent() {
+  // If Bitcoin node is still syncing, we return null and render the "Waiting for Bitcoin Node to finish syncing..." message in the frontend
+  // This way, the sync percent is not calculated until the Bitcoin node is done syncing
   const { initialblockdownload } = await bitcoindService.getBlockChainInfo();
   if (initialblockdownload) {
     return null;
   }
 
   const info = await electrumClient.request('getinfo');
-  const dbHeight = info['db height'];
-  const daemonHeight = info['daemon height'];
+  const dbHeight = info['db height']; // ElectrumX height
+  const daemonHeight = info['daemon height']; // Bitcoin node height
 
   // returns NaN if daemonHeight is 0, which is falsy and caught by the frontend appropriately
   return Math.ceil((dbHeight / daemonHeight) * 100);
